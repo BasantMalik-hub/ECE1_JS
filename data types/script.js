bba2f640e4c8b110f18f1081afd47e5b779b21e7
BasantMalik-hub <chbasantmalik@gmail.com>
2026-02-25 09:51:17 +0530

    dataTypes

---
+++ data types/script.js
@@ -0,0 +1,23 @@
+// console.log("Hello World");
+
+// string
+var name = "Basant Malik";
+console.log(name);
+console.log(typeof(name));
+// Number
+var num = 32;
+console.log(num);
+console.log(typeof(num));
+// Boolean
+num = false;
+console.log(num);
+console.log(typeof(num));
+// undefined
+var number
+console.log(number);
+console.log(typeof(number));
+// null
+var nest = null;
+console.log(nest)
+console.log(typeof(nest));
+
