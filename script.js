3d090cd783c7a2fadb6b271e1baa00eb9162aaa1
BasantMalik-hub <chbasantmalik@gmail.com>
2026-03-11 09:44:34 +0530

    JS

---
+++ script.js
@@ -0,0 +1,43 @@
+// let count =0;
+// document.write("starting loop"+"<br>");
+// for(count =0;count<10;count++){
+//     document.write("current count:" + count);
+//         document.write("<br>")
+// }
+// console.log("loop started");
+
+// for(key in object){
+
+// }
+// const person ={
+//     fname: "Ajay",
+//     lname: "singh",
+//     age: 25
+// };
+// for(let x in person){
+//     console.log("person details "+x+": "+person[x])
+       
+// }
+
+// var age = 20;
+// if (age>18){
+//     document.write("<b>you are elgible to cast the vote</b>");
+// }
+// var age = 17;
+// if (age>18){
+//     document.write("<b>you are elgible to cast the vote</b>");
+// }else{
+//     document.write("<b>you are minor</b>");
+// }
+
+var grade = 'A';
+document.write("entering switch blocks<br/>");
+switch(grade){
+    case 'A' : document.write("Good job<br/>");
+
+     case 'B' : document.write("Pretty job<br/>");
+     case 'C' : document.write("Passed<br/>");
+      case 'D' : document.write("do better next time<br/>");
+       default : document.write("you can do it<br/>");
+        
+}
